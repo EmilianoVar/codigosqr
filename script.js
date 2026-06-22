96f87ad65888f53e8ae5b1adb072bbcb23adddef
Your Name <you@example.com>
2026-06-22 01:13:19 +0000

    Conectar los elementos principales de la interfaz mediante JavaScript para permitir la interacción con el sistema

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const qrInput = document.getElementById("qrInput");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+const resultado = document.getElementById("resultado");
+const urlDetectada = document.getElementById("urlDetectada");
+const diagnostico = document.getElementById("diagnostico");
+const abrirBtn = document.getElementById("abrirBtn");
+
+let urlFinal = ""; 
